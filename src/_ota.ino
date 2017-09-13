0e1c6b77a6720a6baf42a38735d02ff8433253ae
Sebastian Andersson <sebastian@bittr.nu>
2017-09-13 20:15:11 +0200

    Added code to automaticly upgrade from the web.
    The code is commented away until an URL for it is found.

--- src/_ota.ino
+++ src/_ota.ino
@@ -13,10 +13,13 @@
  * Copyright (c) 2016 - 2017 Sacha Telgenhof
  */
 
+#include <ESP8266httpUpdate.h>
+
 /**
  * @brief Bootstrap function for the OTA UDP service
  */
 void setupOTA() {
+
   ArduinoOTA.setPort(OTA_PORT);
   ArduinoOTA.setHostname(cfg.hostname);
   ArduinoOTA.setPassword(ADMIN_PASSWORD);
@@ -29,7 +32,7 @@ void setupOTA() {
     events.send("start", "ota");
 
     ws.enable(false); // Disable WebSocket client connections
-    ws.closeAll(); // Close WebSocket client connections    
+    ws.closeAll(); // Close WebSocket client connections
   });
 
   ArduinoOTA.onEnd([]() {
@@ -66,4 +69,18 @@ void setupOTA() {
 /**
  * @brief Listen to OTA requests
  */
-void loopOTA() { ArduinoOTA.handle(); }
+void loopOTA() {
+  ArduinoOTA.handle();
+
+/*
+  // After 20 seconds, check once for a new firmware.
+  static t_httpUpdate_return ret = (t_httpUpdate_return)-1;
+  if((ret != HTTP_UPDATE_NO_UPDATES) &&
+     (ret != HTTP_UPDATE_FAILED) &&
+     (millis() > 20000)) {
+    ESPhttpUpdate.rebootOnUpdate(true);
+    // https://github.com/bofh69/AiLight/releases/download/0.0.2/firmware.bin
+    ret = ESPhttpUpdate.update("http://192.168.0.16:8080/from-" APP_VERSION "/firmware.bin");
+  }
+*/
+}
